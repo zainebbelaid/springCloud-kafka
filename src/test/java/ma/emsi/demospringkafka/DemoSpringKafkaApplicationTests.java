aa83b2fe8732370e3bc1e676665fb9a43f94a2cc
zainebbelaid <belaid.zaine01@gmail.com>
2024-01-28 20:52:25 +0100

    Initialisation git

---
+++ src/test/java/ma/emsi/demospringkafka/DemoSpringKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.demospringkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
